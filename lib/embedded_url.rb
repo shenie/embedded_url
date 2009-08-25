98f8babf0ad35cc7b8a943dc7d4fbc8265c874ea
Andy Shen <andy@shenie.info>
2009-08-25 19:15:11 +1000

    fixed error with integration

--- lib/embedded_url.rb
+++ lib/embedded_url.rb
@@ -5,3 +5,31 @@ require File.join(File.dirname(__FILE__), 'embedded_url/gist')
 require File.join(File.dirname(__FILE__), 'embedded_url/slideshare')
 require File.join(File.dirname(__FILE__), 'embedded_url/youtube')
 require File.join(File.dirname(__FILE__), 'embedded_url/vimeo')
+
+module EmbeddedURL
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+
+    def with_embedded(field)
+      
+      define_method("embedded_#{field}") do
+
+        link = send(field)
+        
+        begin
+          Gist.new(link).to_embedded || 
+            Youtube.new(link).to_embedded || 
+            Vimeo.new(link).to_embedded || 
+            SlideShare.new(link).to_embedded
+        rescue
+        end
+        
+      end
+      
+    end
+
+  end
+end
--- rails/init.rb
+++ rails/init.rb
@@ -1,31 +1,5 @@
 require "embedded_url"
 
-module EmbeddedUrl
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
-  
-  module ClassMethods
-
-    def embedded_url(field)
-      
-      define_method("embedded_#{field}") do
-
-        link = send(field)
-        
-        begin
-          Gist.new(link).to_embedded || Youtube.new(link).to_embedded || Vimeo.new(link).to_embedded || 
-            SlideShare.new(link).to_embedded ||
-        rescue
-        end
-        
-      end
-      
-    end
-
-  end
-end
-
 ActiveRecord::Base.class_eval do
   include EmbeddedURL
 end
